8fab5a31a5ce6fe37d1d33eff1eb9f36d3e9111a
jaydenSalazar <75498297+jaydenSalazar@users.noreply.github.com>
2021-06-04 13:06:14 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,49 @@
+$(".no-btn").click(function() {
+    $(".yes-btn").hide();
+    $(".no-btn").hide();
+    $(".house").hide();
+    $("h1").hide();
+    $(".crying").show();
+});
+
+
+$(".yes-btn").click(function() {
+    $(".yes-btn").hide();
+    $(".no-btn").hide();
+    $(".house").hide();
+    $("h1").hide();
+    $(".main").show();
+    $(".two").show();
+    $(".ice-cream").show();
+     $("h2").show();
+    $("h3").show();
+});
+
+
+//a choclate cone is slytherin  
+let choclate= slytherin;
+let vinilla= gryffindor;
+let strawberry= hufflepuff;
+let banana= ravenclaw;
+
+let slytherin;
+let hufflepuff;
+let ravenclaw;
+let gryffindor;
+
+$(".to-pack-button").click( function () {
+    console.log("jayden");
+  var number = $(".pizza").val();
+    console.log({number});
+     var icecream = $(".icecream").val();
+    console.log({icecream});
+ });
+                                          
+
+
+
+
+
+
+
+
